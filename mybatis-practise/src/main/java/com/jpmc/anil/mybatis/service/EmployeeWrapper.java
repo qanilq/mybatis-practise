267ced4e65b27cdcf1c18adb3372d5b1ad03de6c
anil.meesala <anil.meesala@hcl.com>
2016-03-21 23:22:26 +0530

    Remove test comments

--- mybatis-practise/src/main/java/com/jpmc/anil/mybatis/service/EmployeeWrapper.java
+++ mybatis-practise/src/main/java/com/jpmc/anil/mybatis/service/EmployeeWrapper.java
@@ -1,20 +1,5 @@
 package com.jpmc.anil.mybatis.service;
 
 public class EmployeeWrapper extends Employee<EmployeePK> {
-    // to test generics
-
-    // Thrid comment
-    // Fourth comment
-    // test commit
-    // FifthCommit
-    // Sixth commit
-    // Six-Seven
-    // Seventh Commit
-    // test seventh commit
-    // eighth commit
-    // ninth commit
-    // 9.1 commit
-    // tenth commit
-    // 11th commit
-    // 12 th commit
+    // This is wrapper
 }
